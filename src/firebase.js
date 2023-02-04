92620e3f0dd99f88981e8ca3fb47f3b21d3e8a3a
Iuliia-D <iuliia.derevleva@gmail.com>
2023-02-04 20:41:50 +0200

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApcKgx6e-Zr9dAZ8vZ9qEFVIH3oSSB-o0",
-  authDomain: "vlad-vet.firebaseapp.com",
-  projectId: "vlad-vet",
-  storageBucket: "vlad-vet.appspot.com",
-  messagingSenderId: "454106686785",
-  appId: "1:454106686785:web:caaba7beac0a159e3aadc0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

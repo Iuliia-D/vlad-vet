0d48230e8e42de6783f058cdbccdb20f2cccd8a5
Iuliia-D <iuliia.derevleva@gmail.com>
2023-03-05 19:08:17 +0200

    install "@emailjs/browser" & create form

--- src/components/pages/contacts/Form.js
+++ src/components/pages/contacts/Form.js
@@ -1,7 +1,35 @@
 import { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 import classes from "./Form.module.scss";
 
 const Form = ({ t, changeLanguage, langMenuActive, enteredLanguage }) => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_YOUR_SERVICE_ID,
+        process.env.REACT_APP_YOUR_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_YOUR_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -18,23 +46,16 @@ const Form = ({ t, changeLanguage, langMenuActive, enteredLanguage }) => {
     setMessage(e.target.value);
   };
 
-  const submitFormHandler = (e) => {
-    e.preventDefault();
-    console.log(name, email, message);
-    setName("");
-    setEmail("");
-    setMessage("");
-  };
-
   return (
     <>
       <div className={classes.form_wrapper}>
         <h3>Напишите мне</h3>
-        <form onSubmit={submitFormHandler}>
+        <form ref={form} onSubmit={sendEmail} autoComplete="off">
           <input
             id="username"
             type="text"
             placeholder="Имя"
+            name="name"
             value={name}
             onChange={nameChangeHandler}
           ></input>
@@ -42,6 +63,7 @@ const Form = ({ t, changeLanguage, langMenuActive, enteredLanguage }) => {
             id="email"
             type="text"
             placeholder="E-mail"
+            name="email"
             onChange={emailChangeHandler}
             value={email}
           ></input>
@@ -49,10 +71,12 @@ const Form = ({ t, changeLanguage, langMenuActive, enteredLanguage }) => {
             id="message"
             type="number"
             placeholder="Сообщение"
+            name="message"
             value={message}
             onChange={messageChangeHandler}
           ></textarea>
           <button type={"submit"}>Отправить</button>
+          {/* <input type="submit" value="Send" /> */}
         </form>
       </div>
     </>

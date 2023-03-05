74f29c41d194e4a09cf7e5a51c6b141c25e07373
Iuliia-D <iuliia.derevleva@gmail.com>
2023-03-05 20:34:24 +0200

    error moderation & translition

--- src/components/pages/contacts/Form.js
+++ src/components/pages/contacts/Form.js
@@ -6,33 +6,65 @@ import classes from "./Form.module.scss";
 
 const Form = ({ t, changeLanguage, langMenuActive, enteredLanguage }) => {
   const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_YOUR_SERVICE_ID,
-        process.env.REACT_APP_YOUR_TEMPLATE_ID,
-        form.current,
-        process.env.REACT_APP_YOUR_PUBLIC_KEY
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    setName("");
-    setEmail("");
-    setMessage("");
-  };
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const [successMassage, setSuccessMassage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const enteredName = name;
+    const enteredEmail = email;
+    const enteredMessage = message;
+
+    if (enteredName.trim().length === 0 || enteredMessage.trim().length === 0) {
+      setError({
+        title: t("formSubmit.part1"),
+        message: t("formSubmit.part2"),
+      });
+      return;
+    }
+    if (!enteredEmail.includes("@") && !enteredEmail.includes(".")) {
+      setError({
+        title: t("formSubmit.part3"),
+        message: t("formSubmit.part4"),
+      });
+      return;
+    } else {
+      emailjs
+        .sendForm(
+          process.env.REACT_APP_YOUR_SERVICE_ID,
+          process.env.REACT_APP_YOUR_TEMPLATE_ID,
+          form.current,
+          process.env.REACT_APP_YOUR_PUBLIC_KEY
+        )
+        .then(
+          (result) => {
+            setSuccessMassage({
+              title: t("formSubmit.part5"),
+              message: t("formSubmit.part6"),
+            });
+            console.log(result.text);
+          },
+          (error) => {
+            // setError({
+            //   title: "Error",
+            //   message: "Something went wrong",
+            // });
+            console.log(error.text);
+          }
+        );
+
+      setName("");
+      setEmail("");
+      setMessage("");
+      setError("");
+
+      // console.log(name, email, message);
+      // setError("");
+    }
+  };
 
   const nameChangeHandler = (e) => {
     setName(e.target.value);
@@ -49,20 +81,38 @@ const Form = ({ t, changeLanguage, langMenuActive, enteredLanguage }) => {
   return (
     <>
       <div className={classes.form_wrapper}>
-        <h3>Напишите мне</h3>
+        <h3>{t("form.title")}</h3>
+        {error ? (
+          <div className={classes.form_error}>
+            <p>{error.title}</p>
+            <p>{error.message}</p>
+          </div>
+        ) : (
+          ""
+        )}
+        {successMassage ? (
+          <div className={classes.form_success}>
+            <p>{successMassage.title}</p>
+            <p>{successMassage.message}</p>
+          </div>
+        ) : (
+          ""
+        )}
         <form ref={form} onSubmit={sendEmail} autoComplete="off">
           <input
             id="username"
             type="text"
-            placeholder="Имя"
+            placeholder={t("form.name")}
             name="name"
+            maxLength="100"
             value={name}
             onChange={nameChangeHandler}
           ></input>
           <input
             id="email"
             type="text"
-            placeholder="E-mail"
+            placeholder={t("form.email")}
+            maxLength="100"
             name="email"
             onChange={emailChangeHandler}
             value={email}
@@ -70,12 +120,14 @@ const Form = ({ t, changeLanguage, langMenuActive, enteredLanguage }) => {
           <textarea
             id="message"
             type="number"
-            placeholder="Сообщение"
+            placeholder={t("form.message")}
             name="message"
+            maxLength="2500"
             value={message}
             onChange={messageChangeHandler}
           ></textarea>
-          <button type={"submit"}>Отправить</button>
+
+          <button type={"submit"}>{t("form.send")}</button>
           {/* <input type="submit" value="Send" /> */}
         </form>
       </div>
